bddc9247b1c70db4fc9a3e97429564fffd1f73d8
Arkadiusz Gil <arkadiusz@arkgil.net>
2019-01-02 17:15:32 +0100

    Remove tests for Elixir pre-1.5 features

--- test/test_helper.exs
+++ test/test_helper.exs
@@ -1,18 +1,10 @@
-elixir_vsn = System.version()
 {otp_release, _} = Integer.parse(System.otp_release())
 
 exclude =
-  if Version.match?(elixir_vsn, "~> 1.5") do
-    []
-  else
-    [:elixir_1_5_child_specs]
-  end
-
-exclude =
   if otp_release < 20 do
-    [:dirty_schedulers | exclude]
+    [:dirty_schedulers]
   else
-    exclude
+    []
   end
 
 ExUnit.start(exclude: exclude)
