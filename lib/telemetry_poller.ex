e1219b169160ff9c9e368a2d259b7178596f1076
José Valim <jose.valim@plataformatec.com.br>
2019-04-02 10:11:18 +0200

    Update telemetry_poller.ex

--- lib/telemetry_poller.ex
+++ lib/telemetry_poller.ex
@@ -1,23 +1,66 @@
 defmodule Telemetry.Poller do
   @moduledoc ~S"""
   A time-based poller to periodically dispatch Telemetry events.
+  
+  Poller comes with a built-in poller but also allows you to run
+  your own. Let's see how.
 
-  By default a single poller is started under `Telemetry.Poller` application.
-  It is started with name `Telemetry.Poller.Default` and a default set of VM
-  measurements. It polls all default VM measurements, which is equivalent to:
+  ## Built-in poller
 
-      config :telemetry_poller, :default,
-        vm_measurements: :default # this is the default
+  By default a single poller is started under the `Telemetry.Poller` application.
+  It is started with name `Telemetry.Poller.Default` and its goal is to perform
+  common VM measurements.
+
+  By default, it polls all default VM measurements, which is equivalent to:
+
+      # This is the default - you don't have to declare it
+      config :telemetry_poller, :default, vm_measurements: :default
 
   But you may specify all VM measurements you want:
 
       config :telemetry_poller, :default,
         vm_measurements: [:memory] # measure only the memory
 
-  Measurements are MFAs called periodically by the poller process.
   You can disable the default poller by setting it to `false`:
 
       config :telemetry_poller, :default, false
+
+  Let's take a closer look at the available measurements.
+
+  ### Memory (default)
+
+  An event emitted as `[:vm, :memory]`. The measurement includes all the key-value
+  pairs returned by `:erlang.memory/0` function, e.g. `:total` for total memory,
+  `:processes_used` for memory used by all processes, etc.
+
+  ### Run queue lengths (default)
+
+  On startup, the Erlang VM starts many schedulers to do both IO and CPU work. If a process
+  needs to do some work or wait on IO, it is allocated to the appropriate scheduler. The run
+  queue is a queue of tasks to be scheduled. A length of a run queue corresponds to the amount
+  of work accumulated in the system. If a run queue length is constantly growing, it means that
+  the BEAM is not keeping up with executing all the tasks.
+
+  There are several run queue types in the Erlang VM. Each CPU scheduler (usually one per core)
+  has its own run queue, and since Erlang 20.0 there is one dirty CPU run queue, and one dirty
+  IO run queue.
+
+  The run queue length event is emitted as `[:vm, :total_run_queue_lengths]`. The event contains
+  no metadata and three measurements:
+
+      * `:total` - a sum of all run queue lengths;
+      * `:cpu` - a sum of CPU schedulers' run queue lengths, including dirty CPU run queue length
+      on Erlang >= 20.0;
+      * `:io` - length of dirty IO run queue. It's always 0 if running on Erlang < 20.0.
+
+    Note that the method of making this measurement varies between different Erlang versions: for
+    Erlang 18 and 19, the implementation is less efficient than for version 20 and up.
+
+    The length of all queues is not gathered atomically, so the event value does not represent
+    a consistent snapshot of the run queues' state. However, the value is accurate enough to help
+    to identify issues in a running system.
+
+  ## Custom pollers
 
   Telemetry poller also allows you perform custom measurements by spawning
   your own poller process:
@@ -35,56 +78,7 @@ defmodule Telemetry.Poller do
   For all options, see `start_link/1`. The options listed there can be given
   to the default poller as well as to custom pollers.
 
-  Over the next sections, we will describe all built-in VM measurements and
-  explore some concrete examples.
-
-  ## VM measurements
-
-  VM measurements need to be provided via `:vm_measurements` option. Below you
-  can find a list of available measurements per category.
-
-  ### Memory
-
-  There is only one measurement related to memory - `:memory`. The emitted event includes all the
-  key-value pairs returned by `:erlang.memory/0` function, e.g. `:total` for total memory,
-  `:processes_used` for memory used by all processes etc.
-
-  ### Run queue lengths
-
-  On startup, the Erlang VM starts many schedulers to do both IO and CPU work. If a process
-  needs to do some work or wait on IO, it is allocated to the appropriate scheduler. The run
-  queue is a queue of tasks to be scheduled. A length of a run queue corresponds to the amount
-  of work accumulated in the system. If a run queue length is constantly growing, it means that
-  the BEAM is not keeping up with executing all the tasks.
-
-  There are several run queue types in the Erlang VMe. Each CPU scheduler (usually one per core)
-  has its own run queue, and since Erlang 20.0 there is one dirty CPU run queue, and one dirty
-  IO run queue.
-
-  The following VM measurements related to run queue lengths are available:
-
-  * `:total_run_queue_lengths` - dispatches an event with a sum of schedulers' run queue lengths. The
-    event name is `[:vm, :total_run_queue_lengths]`, event metadata is empty and it includes three
-    measurements:
-    * `:total` - a sum of all run queue lengths;
-    * `:cpu` - a sum of CPU schedulers' run queue lengths, including dirty CPU run queue length
-      on Erlang >= 20.0;
-    * `:io` - length of dirty IO run queue. It's always 0 if running on Erlang < 20.0.
-
-
-    Note that the method of making this measurement varies between different Erlang versions: for
-    Erlang 18 and 19, the implementation is less efficient than for version 20 and up.
-
-    The length of all queues is not gathered atomically, so the event value does not represent
-    a consistent snapshot of the run queues' state. However, the value is accurate enough to help
-    to identify issues in a running system.
-
-  ### Default measurements
-
-  When `:default` is provided as the value of `:vm_measurement` options, Poller uses `:memory` and
-  `:total_run_queue_lengths` VM measurements.
-
-  ## Example - measuring message queue length of the process
+  ### Example - measuring message queue length of the process
 
   Measuring process' message queue length is a good way to find out if and when the
   process becomes the bottleneck. If the length of the queue is growing, it means that
@@ -181,7 +175,7 @@ defmodule Telemetry.Poller do
   that the worker is the system's bottleneck. In a healthy situation the message queue length would
   be roughly constant.
 
-  ## Example - tracking number of active sessions in web application
+  ### Example - tracking number of active sessions in web application
 
   Let's imagine that you have a web application and you would like to periodically measure number
   of active user sessions.
@@ -271,8 +265,8 @@ defmodule Telemetry.Poller do
   * `:measurements` - a list of measurements used by Poller. For description of possible values
     see `Telemetry.Poller` module documentation;
   * `:vm_measurements` - a list of atoms describing measurements related to the Erlang VM, or an
-    atom `:default`, in which case default VM measurements are used. See "VM measurements" section in
-    the module documentation for more information. Default value is `[]`;
+    atom `:default`, in which case default VM measurements are used. VM measurements are typically
+    performed only by the built-in poller. For custom pollers, the default value is `[]`;
   * `:period` - time period before performing the same measurement again, in milliseconds. Default
     value is #{@default_period} ms;
   * `:name` - the name of the Poller process. See "Name Registragion" section of `GenServer`
