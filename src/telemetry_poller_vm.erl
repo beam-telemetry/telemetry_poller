ce652beb689cb3fda18f61f78a00a0cfc9d46904
Bryan Naegele <bryan@fourthtime.com>
2019-05-28 22:03:43 -0600

    Define cpu_stats at compile time based on OTP version

--- src/telemetry_poller_vm.erl
+++ src/telemetry_poller_vm.erl
@@ -10,30 +10,27 @@ memory() ->
   Measurements = erlang:memory(),
   telemetry:execute([vm, memory], maps:from_list(Measurements), #{}).
 
-%% Need to figure out how to find OTP release version in erlang
 -spec total_run_queue_lengths() -> ok.
 total_run_queue_lengths() ->
-  OTPRelease = otp_version(),
-  Total = cpu_stats(total, OTPRelease),
-  CPU = cpu_stats(cpu, OTPRelease),
+  Total = cpu_stats(total),
+  CPU = cpu_stats(cpu),
   telemetry:execute([vm, total_run_queue_lengths], #{
     total => Total,
     cpu => CPU,
     io => Total - CPU},
     #{}).
 
--spec otp_version() -> non_neg_integer().
-otp_version() ->
-  __Version = erlang:system_info(otp_release),
-  {Version, _} = string:to_integer(__Version),
-  Version.
-
--spec cpu_stats(total | cpu, non_neg_integer()) -> non_neg_integer().
-cpu_stats(total, OTPVersion) when OTPVersion < 20 ->
-  lists:sum(erlang:statistics(run_queue_lengths));
-cpu_stats(total, _) ->
-  erlang:statistics(total_run_queue_lengths_all);
-cpu_stats(cpu, OTPVersion) when OTPVersion < 20 ->
-  lists:sum(erlang:statistics(run_queue_lengths));
-cpu_stats(cpu, _) ->
-  erlang:statistics(total_run_queue_lengths).
+-ifdef(OTP_RELEASE).
+  -if(?OTP_RELEASE >= 20).
+    -spec cpu_stats(total | cpu) -> non_neg_integer().
+    cpu_stats(total) ->
+      erlang:statistics(total_run_queue_lengths_all);
+    cpu_stats(cpu) ->
+      erlang:statistics(total_run_queue_lengths).
+  -endif.
+-else.
+  % OTP_RELEASE macro is available in OTP 19+ so this covers 18 & 19
+  -spec cpu_stats(total | cpu) -> non_neg_integer().
+    cpu_stats(_) ->
+      lists:sum(erlang:statistics(run_queue_lengths)).
+-endif.
