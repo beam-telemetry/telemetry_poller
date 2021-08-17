8ef1be87dbed3d67fe210998b0e403c57d61fafc
Arek Gil <arkadiusz@arkgil.net>
2021-08-17 21:31:00 +0200

    Release 1.0.0 (#62)

--- docs.sh
+++ docs.sh
@@ -9,7 +9,7 @@ set -e
 
 rebar3 as docs compile
 rebar3 as docs edoc
-version=0.5.1
+version=1.0.0
 ex_doc "telemetry_poller" $version "_build/docs/lib/telemetry_poller/ebin" \
   --paths "_build/docs/lib/*/ebin" \
   --source-ref v${version} \
