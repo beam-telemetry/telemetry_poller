1bd58dde3152d0241be669b4cb65c7e013188ca0
Kian-Meng Ang <kianmeng@cpan.org>
2025-10-18 03:53:11 +0800

    Support OTP/28 in CI
    Also bump actions/checkout, bump rebar3, resolve dialyzer error, and add
    missing dialyzer specs.

--- src/telemetry_poller_sup.erl
+++ src/telemetry_poller_sup.erl
@@ -8,9 +8,11 @@
 
 -define(SERVER, ?MODULE).
 
+-spec start_link([supervisor:child_spec()]) -> {ok, pid()}.
 start_link(PollerChildSpec) ->
     supervisor:start_link({local, ?SERVER}, ?MODULE, PollerChildSpec).
 
+-spec init([supervisor:child_spec()]) -> {ok, {supervisor:sup_flags(), [supervisor:child_spec()]}}.
 init(PollerChildSpec) ->
     SupFlags = #{strategy => one_for_one,
                  intensity => 1,
